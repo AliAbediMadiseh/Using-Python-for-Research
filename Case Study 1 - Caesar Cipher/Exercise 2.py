d4be8fb551bdb9dcd01b440c180d78d5fb924f79
aam7596 <63880427+aam7596@users.noreply.github.com>
2020-04-18 15:55:32 +0430

    Update Exercise 2.py

--- Case Study 1 - Caesar Cipher/Exercise 2.py
+++ Case Study 1 - Caesar Cipher/Exercise 2.py
@@ -5,3 +5,14 @@ encryption_key = 3
 
 # define `encoding` here!
 encoding = {alphabet[i] : (i + encryption_key) % 27 for i in range(27)}
+
+
+#the answer in website is:
+positions = {}
+index = 0
+for char in alphabet:
+    positions[char] = index
+    index += 1
+    
+print(positions['n'])
+14
